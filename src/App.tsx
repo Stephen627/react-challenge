d4f8e09bf57d79cb19068b756ea7ff12df4d4192
Stephen James <me@stephenjames.dev>
2022-11-15 20:09:26 +0000

    Add working search

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useMemo } from 'react'
+import Dropdown, { Option } from './components/dropdown/Dropdown'
+import { fetchBreeds, fetchRandomDogs, transformName } from './service/dog'
+import { Status } from './interface/dog.d'
+import Loading from './components/loading/Loading'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [ error, setError ] = useState<string>('')
+  const [ breeds, setBreeds ] = useState<{[key: string]: string[]}>({})
+  const [ selectedBreed, setSelectedBreed ] = useState<string>('')
+  const [ selectedSubBreed, setSelectedSubBreed ] = useState<string>('')
+  const [ selectedNumberOfImages, setSelectedNumberOfImages ] = useState<string>('1')
+  const [ loading, setLoading ] = useState<boolean>(true)
+  const [ images, setImages ] = useState<string[]>([])
+
+  useEffect(() => {
+    fetchBreeds().then(data => {
+      if (data === null || data.status === Status.ERROR) {
+        setError('Something went wrong, please refresh to try again')
+      }
+
+      setBreeds(data.message)
+      setLoading(false)
+    })
+  }, [])
+
+  const breedOptions: any[] = Object.keys(breeds).map(breed => {
+    return {
+      name: transformName(breed),
+      value: breed,
+    }
+  })
+
+  const subBreedOptions = useMemo(() => {
+    const subBreeds = selectedBreed && breeds[selectedBreed] && breeds[selectedBreed].length ? breeds[selectedBreed] : []
+    return subBreeds.map(subBreed => {
+      return {
+        name: transformName(subBreed),
+        value: subBreed,
+      }
+    })
+  }, [ selectedBreed ])
+
+  const getImages = () => {
+    fetchRandomDogs(selectedBreed, parseInt(selectedNumberOfImages), selectedSubBreed).then(res => {
+      setImages(res.message)
+    })
+  }
+
+  if (error) {
+    return <div>{error}</div>
+  }
+
+  if (loading) {
+    return <Loading />
+  }
+
+  const imageJsx = images.map(src => {
+      return <img src={src} />
+  })
+
+  const numberOfImages: Option[] = [
+    { name: '1', value: '1' },
+    { name: '2', value: '2' },
+    { name: '3', value: '3' },
+    { name: '4', value: '4' },
+    { name: '5', value: '5' },
+    { name: '6', value: '6' },
+  ]
+
+  return <div className="App">
+    <header>
+      <Dropdown 
+        options={breedOptions}
+        selectedOption={selectedBreed}
+        onChange={setSelectedBreed}
+      />
+      {subBreedOptions.length > 0 &&
+        <Dropdown
+          options={subBreedOptions}
+          selectedOption={selectedSubBreed}
+          onChange={setSelectedSubBreed}
+        />
+      }
+      <Dropdown
+        options={numberOfImages}
+        selectedOption={selectedNumberOfImages}
+        onChange={setSelectedNumberOfImages}
+      />
+      <input type={'submit'} value={'View Images'} onClick={getImages} />
+    </header>
+    <div>
+      {imageJsx}
     </div>
-  );
+  </div>
 }
 
 export default App;
--- src/service/dog.ts
+++ src/service/dog.ts
@@ -1,7 +1,7 @@
-import { ListBreedsResponse } from '../interface/dog'
+import { ListBreedsResponse, RandomDogImagesResponse } from '../interface/dog'
 const ROOT_URL = 'https://dog.ceo/api/'
 const LIST_BREEDS_URL = ROOT_URL + 'breeds/list/all'
-const URL = ''
+const FETCH_IMAGES_URL = ROOT_URL + 'breed/{breed}/images/random/{amount}'
 
 export const transformName = (breed: string): string => {
   return breed.charAt(0).toUpperCase() + breed.slice(1)
@@ -12,8 +12,12 @@ export const fetchBreeds = (): Promise<ListBreedsResponse> => {
     .then(res => res.json())
 }
 
-export const fetchRandomDogs = (): void => {
-  fetch(URL)
+export const fetchRandomDogs = (breed: string, amount: number, subBreed?: string): Promise<RandomDogImagesResponse> => {
+  const concatBreed = breed + (subBreed ? '/' + subBreed : '')
+  const url = FETCH_IMAGES_URL
+    .replace('{breed}', concatBreed)
+    .replace('{amount}', '' + amount)
+
+  return fetch(url)
     .then(res => res.json())
-    .then(res => console.debug(res))
 }
